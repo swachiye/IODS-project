0c6dbd69fbdec5536cf50dcb9f3b4db5d9096c4c
Wachiye <swachiye@ad.helsinki.fi>
2020-11-05 08:31:55 +0200

    Change data name from "data" to "DataLrn14"

--- data/create_learning2014.R
+++ data/create_learning2014.R
@@ -3,14 +3,14 @@
 
 # Introduction to Regerssion modelling and model validation
 
-#Read the learning2014 data and assigh it a new name "data"
-data <- read.table("http://www.helsinki.fi/~kvehkala/JYTmooc/JYTOPKYS3-data.txt", sep="\t", header=TRUE)
-colnames(data)
-# The dimensions of the data
-dim(data)
+#Read the learning2014 data and assigh it a new name "DataLrn14"
+DataLrn14 <- read.table("http://www.helsinki.fi/~kvehkala/JYTmooc/JYTOPKYS3-DataLrn14.txt", sep="\t", header=TRUE)
+colnames(DataLrn14)
+# The dimensions of the DataLrn14
+dim(DataLrn14)
 
-# The structure of the data
-str(data)
+# The structure of the DataLrn14
+str(DataLrn14)
 
 # Access the dplyr library
 library(dplyr)
@@ -21,27 +21,27 @@ surface_questions <- c("SU02","SU10","SU18","SU26", "SU05","SU13","SU21","SU29",
 strategic_questions <- c("ST01","ST09","ST17","ST25","ST04","ST12","ST20","ST28")
 
 # select the columns related to deep learning and create column 'deep' by averaging
-deep_columns <- select(data, one_of(deep_questions))
-data$Deep <- rowMeans(deep_columns)
+deep_columns <- select(DataLrn14, one_of(deep_questions))
+DataLrn14$Deep <- rowMeans(deep_columns)
 
 # select the columns related to surface learning and create column 'surf' by averaging
-surface_columns <- select(data, one_of(surface_questions))
-data$Surf <- rowMeans(surface_columns)
+surface_columns <- select(DataLrn14, one_of(surface_questions))
+DataLrn14$Surf <- rowMeans(surface_columns)
 
 # select the columns related to strategic learning and create column 'stra' by averaging
-strategic_columns <- select(data, one_of(strategic_questions))
-data$Stra <- rowMeans(strategic_columns)
+strategic_columns <- select(DataLrn14, one_of(strategic_questions))
+DataLrn14$Stra <- rowMeans(strategic_columns)
 
-str(data)
+str(DataLrn14)
 
 # Assess the new column names 
-colnames(data)
+colnames(DataLrn14)
 
 # select the columns to keep
 new_columns <- c("gender","Age","Attitude", "Deep", "Stra", "Surf", "Points")
 
 # select the 'new_columns' to create a new dataset
-learning2014 <- select(data, one_of(new_columns))
+learning2014 <- select(DataLrn14, one_of(new_columns))
 
 # Change "gender into "Gender". To have all column names start with a capital
 colnames(learning2014)[1] = "Gender"
@@ -56,13 +56,13 @@ str(learning2014)
 write.csv(learning2014, "learning2014.csv") 
 
 # Read the new dataset "learning2014.csv"
-data <- read.csv("learning2014.csv")
+NewDataLrn14 <- read.csv("learning2014.csv")
 
 # Assess the top values of the datasets
-head(learning2014)
+head(NewDataLrn14)
 
 #Look at the structure
-str(learning2014)
+str(NewDataLrn14)
 
 
 
