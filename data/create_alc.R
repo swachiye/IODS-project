94d78aac2c9efe53b96ba968ae2db503ad802aa1
Wachiye <swachiye@ad.helsinki.fi>
2020-11-12 15:25:04 +0200

    Upload Data

---
+++ data/create_alc.R
@@ -0,0 +1,9 @@
+#Name: Sheila Wachiye
+#Date: 12/11/2020
+
+# File data: Student Performance Data
+
+# Read data
+
+Math <- read.table("student-mat.csv",sep=";",header=TRUE)
+Por <- read.table("student-por.csv",sep=";",header=TRUE)
