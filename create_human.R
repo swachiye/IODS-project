bff0c80daadae0d8e660a26e2592fdea7b009c93
Wachiye <swachiye@ad.helsinki.fi>
2020-11-19 22:46:31 +0200

    Save

--- create_human.R
+++ create_human.R
@@ -1,45 +1,43 @@
+## Setting up the scene and loading the packages
+rm(list=ls())
 library(readxl)
 library(base)
 library(plyr)
-# Load Data
+library(dplyr)
 
-# Human development
+## Loading Human development data
 hd <- read.csv("http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/human_development.csv", 
                stringsAsFactors = F)
-# Assess the data structure and dimnesion
+# Exploring  the data structure and dimension
 str(hd)
 dim(hd)
 summary(hd)
 
 
-#Gender inequality
+## Loading the Gender inequality data
 gii <- read.csv("http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/gender_inequality.csv", 
                 stringsAsFactors = F, na.strings = "..")
-# Assess the data
+# Exploring the gender data structure and dimension
 str(gii)
 dim(gii)
 head(gii)
 summary(gii)
 
 
-### change the column names
+## Simplifying the column names
 colnames(hd) <- c("Rank", "Country", "HDI", "LEAB", "EYE", "MYE", "GNI", "GNI-HDI") 
 colnames(gii) <- c("Rank", "Country", "GII", "MMR", "ABR", "PRP", "edu2F", "edu2M", "labF", "labM") 
 
 
-
-# Mutate the “Gender inequality” data and create two new variables. 
-#The first one should be the ratio of Female and Male populations with secondary education in each country
+## Mutate the Gender inequality data to create two new variables. 
+# Ratio of Female and Male populations with secondary education in each country
 gii <- mutate(gii, edu_ration=  edu2F / edu2M)
-
-# the ratio of labour force participation of females and males in each country
+# Ratio of labour force participation of females and males in each country
 gii <- mutate(gii, lab_ration=  labF / labM)
 
 
+## Join together the two datasets using the variable Country 
+human <- inner_join(gii, hd, by="Country")
 
-#Join together the two datasets using the variable Country as the identifier. Keep only the countries in both data sets (Hint: inner join). 
-#The joined data should have 195 observations and 19 variables. Call the new joined data "human" and save it in your data folder. (1 point)
-
-# Join two data sets using common variables as (student) identifiers
-join_by <- c("school","sex","age","address","famsize","Pstatus","Medu","Fedu","Mjob","Fjob","reason","nursery","internet")
-
+## Confirming that the dimension of human dataframe is 195 rows and 19 variables
+dim(human)
